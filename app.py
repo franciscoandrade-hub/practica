b73226b3bfe1e236cfc7ebaef9542c4d71f877e6
FranciscoA <francisco.andrade@uttt.edu.mx>
2026-09-22 11:45:30 -0600

    corrigiendo variable1

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-nombre = "Estudiante Anonimizado"
-return f"<h1>Bienvenido al portal universitario, {nombre}!</h1>"
+    nombre = "Estudiante Anonimizado"
+    return f"<h1>Bienvenido al portal universitario, {nombre}!</h1>"
 
 @app.route('/api/status')
 def status():
