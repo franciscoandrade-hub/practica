18f1641b1fc65b1f4365f75df080d089b834c436
FranciscoA <francisco.andrade@uttt.edu.mx>
2026-09-22 10:53:00 -0600

    Estructura Inicial del proyecto

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    # BUG: La variable 'nombre' no está definida, esto causará un error 500 al ejecutarse
+    return f"<h1>Bienvenido al portal universitario, {nombre}!</h1>"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
